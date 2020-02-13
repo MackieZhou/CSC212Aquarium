456a5853696d3b113f0aea6df6d5bf48c4a4df09
MackieZhou <mzhou@smith.edu>
2020-02-13 17:17:10 -0500

    formatted

--- src/main/java/edu/smith/cs/csc212/aquarium/Aquarium.java
+++ src/main/java/edu/smith/cs/csc212/aquarium/Aquarium.java
@@ -6,9 +6,7 @@ import java.util.Random;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-
 import me.jjfoley.gfx.GFX;
 
 /**
@@ -24,33 +22,22 @@ import me.jjfoley.gfx.GFX;
  *
  */
 public class Aquarium extends GFX {
-	
+
 	public static int WIDTH = 500;
 	public static int HEIGHT = 500;
 
-	/**
-	 * Put a snail on the top of the tank.
-	 */
+	// Put a snail on the top of the tank.
 	Snail algorithm = new Snail(177, Snail.HEIGHT, "top");
-
-	/**
-	 * This is a constructor, code that runs when we make a new Aquarium.
-	 */
-	public Aquarium() {
-		// Here we ask GFX to make our window of size WIDTH and HEIGHT.
-		// Don't change this here, edit the variables instead.
-		super(WIDTH, HEIGHT);
-	}
-
+	
 	// create a random number generator
 	public static Random rand = new Random();
 
-	// set a green component of the bgColor
+	// set a green component of the background color
 	public static int green = 0;
 
-	// create arrays colors and fish
-	public static Color[] fishColor = { Color.LIGHT_GRAY, Color.YELLOW, Color.PINK, Color.GRAY, Color.CYAN,
-			Color.WHITE, Color.ORANGE, Color.GREEN, Color.MAGENTA, Color.DARK_GRAY };
+	// create arrays of colors and fish
+	public static Color[] fishColor = { Color.LIGHT_GRAY, Color.YELLOW, Color.PINK, Color.GRAY, Color.CYAN, Color.WHITE,
+			Color.ORANGE, Color.GREEN, Color.MAGENTA, Color.DARK_GRAY };
 	public static Fish[] fish = new Fish[10];
 
 	// create arrays of bubbles
@@ -61,6 +48,12 @@ public class Aquarium extends GFX {
 
 	// create the predator
 	public static Predator Jaws;
+
+	// This is a constructor
+	public Aquarium() {
+		// ask GFX to make our window of size WIDTH and HEIGHT.
+		super(WIDTH, HEIGHT);
+	}
 
 	@Override
 	public void draw(Graphics2D g) {
@@ -89,14 +82,6 @@ public class Aquarium extends GFX {
 		// draw the predator
 		Jaws.drawPredator(g);
 
-		// fuck debugging...
-		System.out.println(Jaws.order);
-		System.out.println("fish pos = " + fish[Jaws.order].x + ", " + fish[Jaws.order].y);
-		System.out.println("dest = " + Jaws.dest_x + ", " + Jaws.dest_y);
-		System.out.println("position = " + Jaws.x + ", " + Jaws.y);
-		System.out.println("speed = " + Jaws.speedx + ", " + Jaws.speedy);
-		
-
 		// the water gets greener and greener
 		if (green < 254 && Snail.eatTime == 0) {
 			green++;
@@ -108,8 +93,6 @@ public class Aquarium extends GFX {
 	public static void main(String[] args) {
 		// Uncomment this to make it go slower!
 		// GFX.FPS = 10;
-		// This is potentially helpful for debugging movement if there are too many
-		// print statements!
 
 		// Note that we can store an Aquarium in a variable of type GFX because Aquarium
 		// is a very specific GFX, much like 7 can be stored in a variable of type int!
@@ -117,6 +100,7 @@ public class Aquarium extends GFX {
 
 		// load the treasure-chest image
 		// URL: https://docs.oracle.com/javase/tutorial/2d/images/loadimage.html
+		// image: https://pngimg.com/download/56542
 		try {
 			img = ImageIO.read(new File("src/main/java/edu/smith/cs/csc212/aquarium/treasureChest.png"));
 		} catch (IOException e) {
@@ -126,7 +110,7 @@ public class Aquarium extends GFX {
 			// determine the size of the fish
 			boolean isLittle = rand.nextBoolean();
 
-			// create the fish objects
+			// create the fish objects with random size
 			fish[i] = new Fish(fishColor[i], isLittle);
 			// create the bubble objects
 			bubbles[i] = new BubbleSystem();
